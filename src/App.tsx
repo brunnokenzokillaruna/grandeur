811816edc2cece9e4be8d78a5feb98720018e0dc
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-02-28 18:42:45 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import MenuPage from './components/MenuPage';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
